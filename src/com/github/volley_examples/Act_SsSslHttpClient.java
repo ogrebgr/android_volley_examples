30de7ae1cdec9dc6f7d7e858d06446cfce2b41f7
Antonio <balduzziantonio85@gmail.com>
2015-02-03 20:27:54 +0100

    Update Act_SsSslHttpClient.java

--- src/com/github/volley_examples/Act_SsSslHttpClient.java
+++ src/com/github/volley_examples/Act_SsSslHttpClient.java
@@ -1,80 +1,82 @@
 /**
-* Copyright 2013 Ognyan Bankov
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2013 Ognyan Bankov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.volley_examples;
-import java.io.InputStream;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.Request.Method;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.volley_examples.R;
-import com.github.volley_examples.toolbox.ExtHttpClientStack;
-import com.github.volley_examples.toolbox.SslHttpClient;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.volley_examples.R;
+import com.github.volley_examples.toolbox.ExtHttpClientStack;
+import com.github.volley_examples.toolbox.SslHttpClient;
+
+import java.io.InputStream;
 /**
-* Demonstrates how to execute HTTPS request against server with self-signed certificate.
-* @author Ognyan Bankov
-*
-*/
+ * Demonstrates how to execute HTTPS request against server with self-signed certificate.
+ * @author Ognyan Bankov
+ *
+ */
 public class Act_SsSslHttpClient extends Activity {
-private TextView mTvResult;
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
-setContentView(R.layout.act__ss_ssl_http_client);
-mTvResult = (TextView) findViewById(R.id.tv_result);
-Button btnSimpleRequest = (Button) findViewById(R.id.btn_simple_request);
-btnSimpleRequest.setOnClickListener(new OnClickListener() {
-@Override
-public void onClick(View v) {
+    private TextView mTvResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act__ss_ssl_http_client);
+        mTvResult = (TextView) findViewById(R.id.tv_result);
+        Button btnSimpleRequest = (Button) findViewById(R.id.btn_simple_request);
+        btnSimpleRequest.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
 // Replace R.raw.test with your keystore
-InputStream keyStore = getResources().openRawResource(R.raw.test);
+                InputStream keyStore = getResources().openRawResource(R.raw.clienttruststore);
 // Usually getting the request queue shall be in singleton like in {@see Act_SimpleRequest}
 // Current approach is used just for brevity
-RequestQueue queue = Volley
-.newRequestQueue(Act_SsSslHttpClient.this,
-new ExtHttpClientStack(new SslHttpClient(keyStore, "test123", 44401)));
-StringRequest myReq = new StringRequest(Method.GET,
-"https://ave.bolyartech.com:44401/https_test.html",
-createMyReqSuccessListener(),
-createMyReqErrorListener());
-queue.add(myReq);
-}
-});
-}
-private Response.Listener<String> createMyReqSuccessListener() {
-return new Response.Listener<String>() {
-@Override
-public void onResponse(String response) {
-mTvResult.setText(response);
-}
-};
-}
-private Response.ErrorListener createMyReqErrorListener() {
-return new Response.ErrorListener() {
-@Override
-public void onErrorResponse(VolleyError error) {
-mTvResult.setText(error.getMessage());
-}
-};
-}
+                RequestQueue queue = Volley
+                        .newRequestQueue(Act_SsSslHttpClient.this,
+                                new ExtHttpClientStack(new SslHttpClient(keyStore, "test", 443)));
+                StringRequest myReq = new StringRequest(Method.GET,
+                        "https://ave.bolyartech.com:44401/https_test.html",
+                        createMyReqSuccessListener(),
+                        createMyReqErrorListener());
+                queue.add(myReq);
+            }
+        });
+    }
+    private Response.Listener<String> createMyReqSuccessListener() {
+        return new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mTvResult.setText(response);
+            }
+        };
+    }
+    private Response.ErrorListener createMyReqErrorListener() {
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTvResult.setText(error.getMessage());
+            }
+        };
+    }
 }
